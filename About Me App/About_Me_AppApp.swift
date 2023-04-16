6b485366359169e24953ddf7bb0ebcc07a8cfe93
Claire Ruttencutter <51419244+cruttencutter@users.noreply.github.com>
2023-04-16 15:42:58 -0500

    Initial Commit

---
+++ About Me App/About_Me_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_AppApp.swift
+//  About Me App
+//
+//  Created by Claire Ruttencutter on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
