f8b721e099bbdb5ceb0577af78e1049ca2dcce59
Claire Ruttencutter <51419244+cruttencutter@users.noreply.github.com>
2023-04-16 17:32:05 -0500

    Initial commit

--- About Me App/ContentView.swift
+++ About Me App/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var learnMore = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("yellow").ignoresSafeArea()
+                
+            VStack(alignment: .center, spacing: 0) {
+                Text("🌟claire🌟")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Image("aboutMeFrame")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                
+                Button("learn more") {
+                    learnMore = true
+                }.font(.body)
+                 .buttonStyle(.borderedProminent)
+                 .tint(.white)
+                 .foregroundColor(Color("green"))
+            }
+            .padding()
+            .background(Image("background")
+                .resizable(resizingMode: .stretch)
+                .cornerRadius(15))
+            .padding()
+            .alert("Introduction", isPresented: $learnMore, actions:{}, message: {
+                Text("Hello! My name is Claire and some of my favorite things include traveling, seeing plays/musicals, art, and hanging out with my cat Goose :) Thanks for checking out my app!")
+            })
         }
-        .padding()
     }
 }
 
